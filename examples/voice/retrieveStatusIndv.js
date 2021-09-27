411acc51a0bd50ed08c42253c0238159875a5cb6
Philip Montgomery <pmontgomery@3c.com>
2021-09-27 16:45:19 -0400

    Updated/Added video retrieve examples

---
+++ examples/voice/retrieveStatusIndv.js
@@ -0,0 +1,13 @@
+const {VoiceClient} = require('cpaas-sdk-node');
+const {AUTH_TOKEN, VOICE_SESSION_ID} = require("../../privateConst");
+
+const client = new VoiceClient(AUTH_TOKEN);
+const request = client.getStatus(VOICE_SESSION_ID)
+
+request
+    .then(res => {
+        console.log(res);
+    })
+    .catch(err => {
+        console.error(err);
+    });
